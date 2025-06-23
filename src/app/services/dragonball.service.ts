36d447b8065fe1b76dc38f43570a40526cbb3822
Anthony Galindo <antoniogalindoalmeida1997@gmail.com>
2025-06-23 14:46:46 -0500

    angular practica Terminada

--- src/app/services/dragonball.service.ts
+++ src/app/services/dragonball.service.ts
@@ -1,20 +1,42 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal , effect} from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+
+const  getCharactersFromLocal =  ():Character[] => {
+const Characters = localStorage.getItem('charactersLocal');
+ return Characters ?  JSON.parse(Characters): [];
+
+}
+
 
 @Injectable({providedIn: 'root'})
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
-    { id: 3, name: 'Piccolo', power: 1500 },
-    // { id: 4, name: 'Yancha', power: 500 },
-  ]);
+
+
+  characters = signal<Character[]>( getCharactersFromLocal() );
 
 
   addCharacter( character : Character ) {
     this.characters.update(
        list => [...list, character]
       );
+
+
   }
+
+  saveCharactersLocal = effect(() => {
+        localStorage.setItem('charactersLocal', JSON.stringify( this.characters() ));
+  });
+
+
+
+
+
 }
+
+
+
+ // { id: 1, name: 'Goku', power: 9001 },
+    // { id: 2, name: 'Vegeta', power: 8000 },
+    // { id: 3, name: 'Piccolo', power: 1500 },
+    // { id: 4, name: 'Yancha', power: 500 },
